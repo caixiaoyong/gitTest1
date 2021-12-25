9aac32bbfeffe952ac4a04850e760d4eac867554
cy <771802291@qq.com>
2021-12-25 23:33:49 +0800

    first commit

---
+++ src/main/java/test.java
@@ -0,0 +1,10 @@
+/**
+ * @author CZY
+ * @date 2021/12/25 23:33
+ * @description test
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("aaa");
+    }
+}
